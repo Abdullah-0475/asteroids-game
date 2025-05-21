94edfa4bf42338ac32033453aa8d1cf469914582
abdullah-0475 <mailabdullahnizamani@gmail.com>
2025-05-22 01:14:36 +0500

    completed player setup

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 import pygame
 from constants import *
-    
+from player import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     fps = pygame.time.Clock()
+    
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updateable,drawable)
+    
     dt = 0
     
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    
+    player = Player(x, y)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
+        
+        updateable.update(dt)
+            
         screen.fill((0,0,0))
-        dt = fps.tick(60) / 1000
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         
+        dt = fps.tick(60) / 1000
 
 
 if __name__ == "__main__":
